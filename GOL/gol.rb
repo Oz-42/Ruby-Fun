851c6ac8e5a7f8be3b829a927059e4bbf6cc9c19
dan-97 <danny.c.ntwari@gmail.com>
2019-05-22 23:21:10 +0200

    Edit GOL/gol.rb
    Edit code and inserted detailed comments

--- GOL/gol.rb
+++ GOL/gol.rb
@@ -15,39 +15,50 @@ class GameOfLife
     place_holder_init(row_size, col_size)
   end
 
+  # Create multi_array variable with value 0, everywhere
   def place_holder_init(row_s,col_s)
     @multi_array = Array.new(row_s) {Array.new(col_s,0)}
   end
 
+  # Display the mulit-arry as grid
   def display_multi_array
     @multi_array.each {|cell| puts "#{cell}"}
   end
 
+  # Insert co-ordinates
   def insert_points(points,row_cap,col_cap)
     @points_array = Array.new(points) {Array.new(2,0)}
 
-    puts "Grid capacity is #{row_cap}x#{col_cap}\n"
+    puts "Insert between: Row=>#{row_cap} Column=>#{col_cap}\n" # indicate to user where to insert points
 
     puts "Insert points where on the above grid?\n"
 
-    for i in 0...points
+    for i in 0...points # for loop used to enter grid co-ordinates for points  
       for u in 0...2
-        print "row #{i} column #{u}: "
-        @points_array[i][u] = gets.chomp.to_i
+        if u == 0
+          print "Point #{i+1} row: " # input value at row for certain point
+          @points_array[i][u] = gets.chomp.to_i # store input value for row into multi-D array
+        else
+          print "Point #{i+1} column: " # input value at column for certain point
+          @points_array[i][u] = gets.chomp.to_i # store input value for column into multi-D array
+        end
       end
     end
 
     @points_array.each {|e| puts "#{e}"}
 
-    insert_at(points)
+    insert_at(points) # function call
+    gets
   end
 
+  # Insert values at specified co-ordinates
   def insert_at(points)
     for i in 0...points
-      @multi_array[@points_array[i][0]][@points_array[i][1]] = 1
+      @multi_array[@points_array[i][0]][@points_array[i][1]] = 1 # insert 1 at specified position
     end
   end
 
+  # Reset grid(multi_array) to value 0, everywhere
   def clear_grid(row_s,col_s)
     for i in 0...row_s
       for u in 0...col_s
@@ -75,6 +86,7 @@ end
   puts "Welcome to CONWAY'S GAME OF LIFE"
 
   print "Input size of grid: "
+  puts "\nExample: 10 10"
   row,col = gets.chomp.split.map(&:to_i)
 
   gol_obj = GameOfLife.new(row,col)
@@ -96,12 +108,12 @@ when 1
   print "\nHow many point: "
   points = gets.chomp.to_i
 
-  gol_obj.insert_points(points,row,col)
+  gol_obj.insert_points(points,row,col) # function call
 
   gol_obj.clear_screen(choice) # method for clearing terminal
 
 when 2
-  gol_obj.clear_grid(row,col)
+  gol_obj.clear_grid(row,col) # function call
 
   gol_obj.clear_screen(choice) # method for clearing terminal
 
